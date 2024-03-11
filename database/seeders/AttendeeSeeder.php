e2053110cb09c322c9b5e19589a00f1bbe9d90ce
Saqib-Islam <saqimughal72@gmail.com>
2024-03-11 14:40:22 +0500

    Event crud has performed

--- database/seeders/AttendeeSeeder.php
+++ database/seeders/AttendeeSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Attendee;
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +13,23 @@ class AttendeeSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
     public function run(): void
     {
-        //
+        $users = User::all();
+        $events = Event::all();
+
+        foreach ($users as $user) {
+            $eventsToAttend = $events->random(rand(1, 3));
+
+
+            foreach ($eventsToAttend as $event) {
+                Attendee::create([
+                    'user_id' => $user->id,
+                    'event_id' => $event->id
+                ]);
+            }
+
+        }
     }
 }
